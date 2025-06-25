2f0320b25bbf9a3a2c3a1f0eb4a73f7a08cb561d
stiratto <moranmoralesj4@gmail.com>
2025-06-24 22:01:24 -0500

    removed send() (moved into response.ts), cors removed (moved into options)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,16 +1,1 @@
-import http from "http"
-
-export const cors = (req: http.IncomingMessage, res: http.ServerResponse) => {
-  if (req.headers.origin === "http://localhost:5173") {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173")
-  }
-}
-
-
-export const send = (res: http.ServerResponse, message: string, status: number) => {
-  res.writeHead(status, { "Content-Type": "application/json" })
-  res.end(JSON.stringify(message))
-}
-
-
 
