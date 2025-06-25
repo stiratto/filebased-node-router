f9ece95a02a93b514b694b11398e24b33d137cd1
stiratto <moranmoralesj4@gmail.com>
2025-06-24 21:55:47 -0500

    server initialization centralized, no new router(), only new server(), cors can be enabled as config object now, headers too

--- index.ts
+++ index.ts
@@ -1,59 +1,8 @@
-import http from "http"
-import { parse } from "url"
-import { WebSocket, WebSocketServer } from "ws"
-import { cors } from "@/lib/utils"
-import { Router } from "@/router"
+import { Server } from "@/server"
 
-// let connectedUsers = new Map<string, WebSocket>()
-
-const router = new Router()
-const server = http.createServer((req, res) => {
-   cors(req, res)
-   router.handleRequest(req, res)
-
+const server = new Server(4000, {
+  cors: {
+    enabled: true,
+    // can be uppercase or lowercase, it'll get formatted properly
+  }
 })
-
-// const wss = new WebSocketServer({ server })
-
-// wss.on('connection', function connection(ws, req) {
-//    const { pathname, query } = parse(req.url!, true)
-//
-//    if (pathname === '/connect') {
-//       console.log("user connected")
-//       const { userId: id } = query
-//       if (id !== 'null') {
-//          connectedUsers.set(id as string, ws)
-//       }
-//
-//    } else if (pathname === "/disconnect") {
-//       const { id } = query
-//       if (id !== 'null') {
-//          connectedUsers.delete(id as string)
-//       }
-//    }
-//
-//
-//    ws.on('message', function message(data: string) {
-//       const parsedData = JSON.parse(data)
-//       const { to, message, userId } = parsedData
-//
-//       console.log(`Message from ${userId} to ${to}: ${message}`)
-//
-//       const receiverSocket = connectedUsers.get(to)
-//
-//       if (receiverSocket) {
-//          console.log("asd")
-//          receiverSocket?.send(JSON.stringify({
-//             type: "new_message",
-//             from: userId,
-//             to,
-//             message,
-//          }))
-//       } else {
-//          console.log("nop nop")
-//       }
-//    })
-// })
-//
-
-server.listen(4000)
